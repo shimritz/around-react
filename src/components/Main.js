82cb479dc38dc27a0f8756a355bda8ef7c8aee97
Shimrit breef Ziskand <shimrit021@gmail.com>
2022-07-20 09:57:41 +0100

    usestate for API data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import profilePhoto from "../images/profile_photo-image(1).jpg";
+import { api } from "../utils/api";
 
 function Main({
   onEditProfileClick,
@@ -7,19 +8,35 @@ function Main({
   onEditAvatarClick,
   onCardClick,
 }) {
+  const [userName, setUserName] = React.useState("");
+  const [userDescription, setUserDescription] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
+
+  React.useEffect(() => {
+    api
+      .getUserInfo()
+      .then((res) => {
+        setUserName(res.name);
+        setUserDescription(res.aboutMe);
+        setUserAvatar(res.avatar);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <main className="page__content">
       <section className="profile">
         <div onClick={onEditAvatarClick} className="profile__avatar-container">
           <img
             className="profile__avatar"
-            src={profilePhoto}
+            // src={profilePhoto}
+            src={userAvatar}
             alt="user photo"
           />
         </div>
         <div className="profile__info">
           <div className="profile__name-container">
-            <h1 className="profile__name"></h1>
+            <h1 className="profile__name">{userName}</h1>
 
             <button
               type="button"
@@ -27,7 +44,7 @@ function Main({
               onClick={onEditProfileClick}
             ></button>
           </div>
-          <p className="profile__about-me">aboutMe</p>
+          <p className="profile__about-me">{userDescription}</p>
         </div>
 
         <button
--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,71 @@ class Api {
     this._baseUrl = baseUrl;
     this._headers = headers;
   }
+
+  _checkResponse = (res) =>
+    res.ok ? res.json() : Promise.reject(res.statusText);
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  createCard(data) {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+      method: "POST",
+      body: JSON.stringify(data),
+    }).then(this._checkResponse);
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      headers: this._headers,
+      method: "DELETE",
+    }).then(this._checkResponse);
+  }
+
+  editProfile(name, about) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+      method: "PATCH",
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  editAvatar(avatar) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      headers: this._headers,
+      method: "PATCH",
+      body: JSON.stringify({
+        avatar: avatar,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  addLike(id) {
+    return fetch(`${this._baseUrl}/cards/likes/${id}`, {
+      headers: this._headers,
+      method: "PUT",
+    }).then(this._checkResponse);
+  }
+
+  removeLike(id) {
+    return fetch(`${this._baseUrl}/cards/likes/${id}`, {
+      headers: this._headers,
+      method: "DELETE",
+    }).then(this._checkResponse);
+  }
 }
 
 export const api = new Api({
