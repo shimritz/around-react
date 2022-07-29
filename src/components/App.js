6d009518cf527cfed3a0e57b82766c4dac7b3b68
Shimrit breef Ziskand <shimrit021@gmail.com>
2022-07-29 12:27:36 +0100

    Create a context object and use the provider

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 import { useState } from "react";
 import { api } from "../utils/api";
+import {
+  CurrentUserContext,
+  currentUser,
+} from "../contexts/CurrentUserContext";
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
@@ -67,94 +71,96 @@ function App() {
 
   return (
     <div>
-      <Header />
-      <Main
-        onEditProfileClick={handleEditProfileClick}
-        onAddPlaceClick={handleAddPlaceClick}
-        onEditAvatarClick={handleEditAvatarClick}
-        onCardClick={handleCardClick}
-      />
-      <ImagePopup
-        card={selectedCard}
-        isOpen={isPreviewImageOpen}
-        onClose={closeAllPopups}
-      ></ImagePopup>
-      <PopupWithForm
-        name="profile"
-        title="Edit profile"
-        isOpen={isEditProfilePopupOpen}
-        onClose={closeAllPopups}
-        buttonText={"Save"}
-      >
-        <input
-          id="name-input"
-          className="form__input form__input_type_name"
-          type="text"
-          name="name"
-          required
-          minLength="2"
-          maxLength="40"
+      <CurrentUserContext.Provider value={currentUser}>
+        <Header />
+        <Main
+          onEditProfileClick={handleEditProfileClick}
+          onAddPlaceClick={handleAddPlaceClick}
+          onEditAvatarClick={handleEditAvatarClick}
+          onCardClick={handleCardClick}
         />
-        <span id="name-input-error"></span>
-        <input
-          id="aboutme-input"
-          className="form__input form__input_type_about-me"
-          type="text"
-          name="aboutMe"
-          required
-          minLength="2"
-          maxLength="200"
-        />
-        <span id="aboutme-input-error"></span>
-      </PopupWithForm>
+        <ImagePopup
+          card={selectedCard}
+          isOpen={isPreviewImageOpen}
+          onClose={closeAllPopups}
+        ></ImagePopup>
+        <PopupWithForm
+          name="profile"
+          title="Edit profile"
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          buttonText={"Save"}
+        >
+          <input
+            id="name-input"
+            className="form__input form__input_type_name"
+            type="text"
+            name="name"
+            required
+            minLength="2"
+            maxLength="40"
+          />
+          <span id="name-input-error"></span>
+          <input
+            id="aboutme-input"
+            className="form__input form__input_type_about-me"
+            type="text"
+            name="aboutMe"
+            required
+            minLength="2"
+            maxLength="200"
+          />
+          <span id="aboutme-input-error"></span>
+        </PopupWithForm>
 
-      <PopupWithForm
-        name="changeAvatar"
-        title="Change avatar"
-        isOpen={isEditAvatarPopupOpen}
-        onClose={closeAllPopups}
-        buttonText={"Save"}
-      >
-        <input
-          className="form__input form__input_type_image"
-          type="url"
-          name="image"
-          id="avatarImage-input"
-          placeholder="Image URL"
-          required
-        />
-        <span id="avatarImage-input-error"></span>
-      </PopupWithForm>
-      <PopupWithForm
-        name="addNewCard"
-        title="New Place"
-        isOpen={isAddPlacePopupOpen}
-        onClose={closeAllPopups}
-        buttonText={"Create"}
-      >
-        <input
-          id="title-input"
-          className="form__input form__input_type_name"
-          type="text"
-          name="title"
-          placeholder="Title"
-          required
-          minLength="1"
-          maxLength="30"
-        />
-        <span id="title-input-error"></span>
-        <input
-          id="image-input"
-          className="form__input form__input_type_image"
-          type="url"
-          name="image"
-          placeholder="Image URL"
-          required
-        />
-        <span id="image-input-error"></span>
-      </PopupWithForm>
+        <PopupWithForm
+          name="changeAvatar"
+          title="Change avatar"
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          buttonText={"Save"}
+        >
+          <input
+            className="form__input form__input_type_image"
+            type="url"
+            name="image"
+            id="avatarImage-input"
+            placeholder="Image URL"
+            required
+          />
+          <span id="avatarImage-input-error"></span>
+        </PopupWithForm>
+        <PopupWithForm
+          name="addNewCard"
+          title="New Place"
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          buttonText={"Create"}
+        >
+          <input
+            id="title-input"
+            className="form__input form__input_type_name"
+            type="text"
+            name="title"
+            placeholder="Title"
+            required
+            minLength="1"
+            maxLength="30"
+          />
+          <span id="title-input-error"></span>
+          <input
+            id="image-input"
+            className="form__input form__input_type_image"
+            type="url"
+            name="image"
+            placeholder="Image URL"
+            required
+          />
+          <span id="image-input-error"></span>
+        </PopupWithForm>
 
-      <Footer />
+        <Footer />
+      </CurrentUserContext.Provider>
     </div>
   );
 }
